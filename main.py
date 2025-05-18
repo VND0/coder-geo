891284d2431b667eaca4be5bd530bb3f96401727
nikita <vnd-08@yandex.ru>
2025-05-18 12:30:26 +0300

    10 аптек

--- main.py
+++ main.py
@@ -2,18 +2,44 @@ import sys
 
 from PIL import Image
 
-from tools import get_feature_size, get_feature, get_image
+from tools import get_features, get_image, get_nearest_features_collection
 
-toponym_to_find = " ".join(sys.argv[1:]) or "Октябрьский проспект 196"
+address = " ".join(sys.argv[1:]) or "Октябрьский проспект 196"
 
-toponym = get_feature(toponym_to_find)
+toponym = get_features(address, 1)[0]
 
 toponym_coordinates = toponym["geometry"]["coordinates"]
 toponym_lon, toponym_lat = map(str, toponym_coordinates)
 pos = ",".join([toponym_lon, toponym_lat])
 
-delta = str(round(get_feature_size(toponym) / 2, 6))
-im = get_image(pos, delta)
+pharmacies = get_nearest_features_collection(
+    pos=pos,
+    results=10,
+    text="Аптека",
+    kind="biz"
+)
+
+bounded_by = pharmacies["properties"]["ResponseMetaData"]["SearchResponse"]["boundedBy"]
+lower_corner = ','.join(map(str, bounded_by[0]))
+upper_corner = ','.join(map(str, bounded_by[1]))
+
+points = [f"{pos},pm2rdl"]
+for feature in pharmacies["features"]:
+    pos = feature["geometry"]["coordinates"]
+
+    try:
+        hours = feature["properties"]["CompanyMetaData"]["Hours"]
+        try:
+            availability = hours["Availabilities"][0]
+            col = "gn" if availability.get("TwentyFourHours", False) else "bl"
+        except (KeyError, IndexError):
+            col = "bl"
+    except KeyError:
+        col = "gr"
+
+    points.append(f"{pos[0]},{pos[1]},pm2{col}m")
+
+im = get_image(bbox=f"{lower_corner}~{upper_corner}", points=points)
 
 opened_image = Image.open(im)
 opened_image.show()
--- tools.py
+++ tools.py
@@ -1,28 +1,30 @@
 import json
 import sys
 from io import BytesIO
+from typing import Literal
 
 import requests
 
 search_api_server = "https://search-maps.yandex.ru/v1/"
 map_api_server = "https://static-maps.yandex.ru/v1"
+search_api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
 
-def get_feature(to_find: str) -> dict:
+def get_features(to_find: str, results: int) -> list[dict]:
     params = {
-        "apikey": "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3",
+        "apikey": search_api_key,
         "text": to_find,
         "lang": "ru_RU",
-        "results": 1
+        "results": results
     }
     response = requests.get(search_api_server, params=params)
 
     json_response = response.json()
-    with open("out.json", "w") as f:
+    with open("out1.json", "w") as f:
         json.dump(json_response, f, ensure_ascii=False)
 
     try:
-        return json_response["features"][0]
+        return json_response["features"]
     except KeyError:
         print("Ничего не найдено", file=sys.stderr)
         exit(1)
@@ -38,13 +40,29 @@ def get_feature_size(feature: dict) -> float:
     )  # Возвращаю максимальную длину прямоугольника boundedBy. Больше я нигде никаких размеров в api не нашел.
 
 
-def get_image(pos: str, delta: str) -> BytesIO:
+def get_image(bbox: str, points: list[str]) -> BytesIO:
     map_params = {
-        "ll": pos,
-        "spn": ",".join([delta, delta]),
+        "bbox": bbox,
         "apikey": "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13",
-        "pt": f"{pos},comma"
+        "pt": "~".join(points)
     }
 
     response = requests.get(map_api_server, params=map_params)
     return BytesIO(response.content)
+
+
+def get_nearest_features_collection(pos: str, results: int, text: str, kind: Literal["geo", "biz"]) -> dict:
+    params = {
+        "apikey": search_api_key,
+        "text": text,
+        "lang": "ru_RU",
+        "type": kind,
+        "ll": pos,
+        "results": results
+    }
+    response = requests.get(search_api_server, params=params)
+
+    json_response = response.json()
+    with open("out2.json", "w") as f:
+        json.dump(json_response, f, ensure_ascii=False)
+    return json_response
